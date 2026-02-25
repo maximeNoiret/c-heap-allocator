c993a32ec0db83964015e8628337468eae0a60ca
NOIRET Maxime <maxime.noiret@etu.univ-amu.fr>
2026-02-25 06:23:17 +0100

    dev: Added little test for init and malloc

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "heap.h"
+
+
+
+int main() {
+  heap_init();
+  
+  size_t *ptr1 = heap_malloc(sizeof(size_t));
+  *ptr1 = 127;
+
+  int *ptr2 = heap_malloc(sizeof(int));
+  *ptr2 = 17;
+
+  char *ptr3 = heap_malloc(14 * sizeof(char));
+  memcpy(ptr3, "Hello, World!", 13);
+  *(ptr3 + 13) = '\0';
+
+  print_heap();
+
+  printf("ptr1 content: %zu\n", *ptr1);
+  printf("ptr2 content: %d\n", *ptr2);
+  printf("ptr3 string:  %s\n", ptr3);
+  return 0;
+}
